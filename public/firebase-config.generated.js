7a9ad1ff686e326bb3d075b3c2269c56a28f1564
Adrian <a.markiewicz@iamcodee.co>
2026-01-15 12:11:16 +0100

    chore: enforce Yarn 4.12.0 across all environments

--- public/firebase-config.generated.js
+++
@@ -1,7 +0,0 @@
-self.FIREBASE_CONFIG = {
-  "apiKey": "AIzaSyDPgIBC6ZPKTarjBhYMMpE-7aQ80WwaLH0",
-  "authDomain": "venloop-88ec5.firebaseapp.com",
-  "projectId": "venloop-88ec5",
-  "messagingSenderId": "337958245713",
-  "appId": "1:337958245713:web:3c378ad6812ddbe771ef70"
-};
